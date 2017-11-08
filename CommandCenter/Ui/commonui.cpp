921a40f4930874c4083b68931ad5cf1fc7e292d5
laidawang <laidawang@zds-t.com>
2017-11-08 14:12:41 +0800

    添加Ui工程，这个工程只做界面，不涉及任何数据和逻辑

---
+++ CommandCenter/Ui/commonui.cpp
@@ -0,0 +1,6 @@
+#include "commonui.h"
+
+
+CommonUi::CommonUi()
+{
+}
---
+++ CommandCenter/Ui/commonui.h
@@ -0,0 +1,13 @@
+#ifndef COMMONUI_H
+#define COMMONUI_H
+
+#include "ui_global.h"
+
+class UISHARED_EXPORT CommonUi
+{
+
+public:
+    CommonUi();
+};
+
+#endif // COMMONUI_H
