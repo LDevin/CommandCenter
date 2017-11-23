8730badee3078aff870ace288e1092359eaf3f3f
laidawang <laidawang@zds-t.com>
2017-11-23 18:07:23 +0800

    modify

--- CommandCenter/Db/dbdefines.h
+++ CommandCenter/Db/dbdefines.h
@@ -63,6 +63,6 @@ qDebug() \
 /**
   * 数据库处理操作出现的错误
 **/
-#define DB_MANAGE_MSG "{\"code\":-200,\"msg\":\"operation is false!\",\"success\":false,\"fail\":true}"
+#define DB_MANAGE_MSG "{\"code\":-200,\"msg\":\"operation is false!\"}"
 
 #endif // DBDEFINES_H
